29fc52db0d5ebf70709776c22fa6d7713785a643
Mokxsh404 <mokxsh404@users.noreply.github.com>
2026-05-08 12:33:57 +0530

    feat(visora): colour detection OpenCV HSV filter

--- VISORA/src/main.cpp
+++ VISORA/src/main.cpp
@@ -1,22 +1,18 @@
-// Visora main code - ESP32-S3 Sense (v2: YOLO TCP Client)
+// Visora main code - ESP32-S3 Sense (v3: HSV Colour Client)
 #include <Arduino.h>
 #include <WiFi.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
 
-Adafruit_ST7735 tft(1, 2, 6, 5, 3); // CS, DC, MOSI, SCLK, RST
-WiFiClient client;
+Adafruit_ST7735 tft(1, 2, 6, 5, 3);
 
 void setup() {
   Serial.begin(115200);
   tft.initR(INITR_BLACKTAB);
   tft.fillScreen(ST77XX_BLACK);
   tft.setCursor(10, 10);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.print("Object detection init");
+  tft.print("Colour detection init");
 }
 
 void loop() {
-  // Connect and read detections
   delay(100);
 }
