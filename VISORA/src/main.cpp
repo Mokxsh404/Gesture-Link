43d215f91b3de0521c446d503a4dab4ce2e16610
Mokxsh404 <mokxsh404@users.noreply.github.com>
2026-05-20 23:02:12 +0530

    feat(visora): I2S audio output via MAX98357A

--- VISORA/src/main.cpp
+++ VISORA/src/main.cpp
@@ -1,16 +1,28 @@
-// Visora main code - ESP32-S3 Sense (v3: HSV Colour Client)
+// Visora main code - ESP32-S3 Sense (v4: MAX98357A I2S Audio Amp)
 #include <Arduino.h>
 #include <WiFi.h>
 #include <Adafruit_ST7735.h>
+#include "driver/i2s.h"
 
-Adafruit_ST7735 tft(1, 2, 6, 5, 3);
+void setupI2SSpeaker() {
+  const i2s_config_t i2s_config = {
+      .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+      .sample_rate = 22050,
+      .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+      .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+      .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+      .intr_alloc_flags = 0,
+      .dma_buf_count = 8,
+      .dma_buf_len = 512,
+      .use_apll = false
+  };
+  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+}
 
 void setup() {
   Serial.begin(115200);
-  tft.initR(INITR_BLACKTAB);
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setCursor(10, 10);
-  tft.print("Colour detection init");
+  setupI2SSpeaker();
+  Serial.println("I2S speaker amp ready.");
 }
 
 void loop() {
